255d6e4d8fc2ae8139c9d558d9f2df0a38f14b47
Schnacken <e.schnackenburg@gmail.com>
2020-02-10 22:03:19 +0100

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { config } from '../assets/config/licence-configs.js';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: [ './app.component.css' ]
 })
-export class AppComponent {
-  title = 'licence-environment-test';
+export class AppComponent implements OnInit {
+	title = 'licence-environment-test';
+	clientName = config.clientName;
+
+	ngOnInit() {}
 }
---
+++ src/assets/config/licence-configs.js
@@ -0,0 +1,3 @@
+export const config = {
+	clientName: 'placeholder client name'
+};
